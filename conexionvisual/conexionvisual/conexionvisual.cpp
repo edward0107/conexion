64a2e4a06fcf6c4b2647cfb7836d19cfba644942
edward <edwarlopez010@gmail.com>
2021-04-10 23:34:37 -0600

    first commit

---
+++ conexionvisual/conexionvisual/conexionvisual.cpp
@@ -0,0 +1,66 @@
+// conexionvisual.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+
+
+int main()
+{
+	MYSQL* conectar;
+
+	conectar = mysql_init(0);
+
+	conectar = mysql_real_connect(conectar, "localhost", "edward", "edwar@123", "db_", 3306, NULL, 0);
+
+	if (conectar) {
+
+		string puesto;
+
+		cout << "Ingrese Puesto:";
+
+		cin >> puesto;
+
+		string insert = "insert into puestos(puesto) values('" + puesto + "')";
+
+		const char* i = insert.c_str();
+
+		q_estado = mysql_query(conectar, i);
+
+		if (!q_estado) {
+
+			cout << "Ingreso Exitoso de la Informacion" << endl;
+
+		}
+
+		else {
+
+			cout << " --- Error al Ingresar ---" << endl;
+
+		}
+
+	}
+
+	else {
+
+		cout << "Error en la Conexion" << endl;
+
+	}
+
+	system("pause");
+
+	return 0;
+
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
